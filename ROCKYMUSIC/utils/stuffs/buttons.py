da5d641a0094074e46188213eac2d1c18723c914
ISTKHAR-CODER786 <istkharalam6202@gmail.com>
2025-12-19 22:05:28 +0530

    Update buttons.py

--- ROCKYMUSIC/utils/stuffs/buttons.py
+++ ROCKYMUSIC/utils/stuffs/buttons.py
@@ -3,62 +3,12 @@ from pyrogram import Client, filters, enums
 
 class BUTTONS(object):
     ABUTTON = [
-
-
-"─────────────────────────"
-❖ ᴧ ϻᴜsɪᴄ ʙσᴛ ғσʀ ᴛєʟєɢʀᴧϻ ɢʀσᴜᴘs / ᴄʜᴧηηєʟs
-─────────────────────────
-● ᴡʀɪᴛᴛєη ɪη ➥ [ᴩʏᴛʜση](https://www.python.org/)
-● ᴅᴧᴛᴧʙᴧsє ➥ [ϻᴏηɢᴏ-ᴅʙ](https://www.mongodb.com/)
-● ʜєʟᴘ ᴡɪᴛʜ [ᴘʏʀᴏɢʀᴧϻ](https://github.com/pyrogram/pyrogram) & [ᴘʏ-ᴛɢᴄᴧʟʟs](https://github.com/pytgcalls/pytgcalls).
-─────────────────────────
-✦ ηᴏ ʟᴀɢ • ηᴏ ᴧᴅs • ηᴏ ʙᴜɢs • 24/7 sᴜᴘᴘᴏʀᴛ
-✦ ᴇηᴊᴏʏ ʟᴧɢ ғʀєє ϻᴜsɪᴄ • ᴧᴅᴅ ϻє ηᴏᴡ
-"─────────────────────────"
-» ᴘʀɪᴠᴧᴛє & sєᴄυʀє sσᴜʀᴄє | ηᴏ ᴅᴧᴛᴧ sʜᴧʀᴇ
-» ʟɪᴄєηᴄєᴅ ᴜsє | ʏσυтυʙє ᴀᴘɪ sυᴘᴘσʀтєᴅ 
-"**──────────────────────"
-➤ ʙσᴛ sᴛᴧᴛυs & ϻσʀє ʙσᴛs - [ᴄʟɪᴄᴋ ʜєʀє](https://t.me/Vibe_Bots)
-➤ ᴘᴧɪᴅ ᴘʀσϻσᴛɪση :- [ᴄσηᴛᴧᴄᴛ ʜєʀє](https://t.me/Itzz_istkhar)
-"**─────────────────────────"
-'''
-    
-    HELP_SUPPORT = '''
-**❖ <u>{0}</u> sᴜᴘᴘᴏʀᴛ ɪηғσ :-
-
-● 24/7 ᴧᴠᴧɪʟᴧʙʟє ғσʀ ᴧηʏ ʙσᴛ.
-● ᴋᴇᴇᴘ ᴄʜᴧᴛ ᴄʟєᴧη ᴧηᴅ ʙᴜɢ-ғʀєє.
-● ᴧηʏ ᴘʀσʙʟєϻ ? ᴊᴜsᴛ ᴄσηᴛᴧᴄᴛ ᴜs.
-─────────────────────────
-❖ υᴘᴅᴧᴛєs ᴄʜᴧηηєʟ ➥ [ʙσᴛs]("https://t.me/Vibe_Bots")
-❖ sυᴘᴘσʀᴛ ᴄʜᴧᴛ ➥ [ᴜᴘᴅᴧᴛєs]("https://t.me/IamIstkhar")
-─────────────────────────**
-'''
-
-HELP_NEW = '''
-<b><u>⊚ ʀᴛᴍᴘ ʟɪᴠᴇ sᴛʀᴇᴀᴍ ᴄᴏᴍᴍᴀɴᴅs :</b></u>
-
-⚙️ `/setrtmp group id rtmp_link` 
-
-• ʟɪηᴋ ʀᴛϻᴘ ᴛᴏ ɢʀσᴜᴘ (ᴘʀɪᴠᴧᴛᴇ ᴄʜᴧᴛ σηʟʏ)  
-• **єx**: `/setrtmp -10020xxx rtmps://xxxx`  
-
-➻ `/playstream song name`  
-
-• **єx**: `/playstream tera fitoor`  
-
-➻ `/endstream` 
-
-• **sᴛσᴘ sᴛʀєᴧϻɪηɢ & ᴄʟєᴧʀ ǫᴜєᴜє** (ɢʀσᴜᴘ ᴧᴅϻɪηs σηʟʏ)  
-  
-
-ℹ️ **ησᴛєs:**  
-
-• ʀᴛϻᴘ ϻᴜsᴛ ʙє sєᴛ ғɪʀsᴛ (ᴜsє `/setrtmp`)  
-• ᴡσʀᴋs ɪη ɢʀσᴜᴘ ᴠσɪᴄє ᴄʜᴧᴛ 
-• ᴧᴅϻɪηs ᴄσηᴛʀσʟ єηᴅ ᴄσϻϻᴧηᴅs 
-• σηʟʏ 1 ʀᴛϻᴘ ʟɪηᴋ ᴘєʀ ɢʀσᴜᴘ
-
-**⊚ ᴄʀᴇᴀᴛᴇᴅ ʙʏ :- [Vibe](t.me/Vibe_Bots)**
-'''
-
+    [
+        InlineKeyboardButton("˹ sυᴘᴘσʀᴛ ˼", url="https://t.me/+IvUyIY_vMY1jOTdl"),
+        InlineKeyboardButton("˹ υᴘᴅᴧᴛєs ˼", url="https://t.me/ARISHFA_UPDATE")
+    ],
+    [
+        InlineKeyboardButton("˹ ʟᴧηɢᴜᴧɢє ˼", callback_data="LG"),
+        InlineKeyboardButton("˹ ʙᴧᴄᴋ ˼", callback_data=f"settingsback_helper")
+    ]
+    ]
